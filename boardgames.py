32ea42d5dd8436ba3fbdd8bda17a0d6491f34e78
Palmmro <palmro@student.chalmers.se>
2017-03-06 01:11:06 +0100

    print random game from list

--- boardgames.py
+++ boardgames.py
@@ -1,5 +1,9 @@
+import random
+import sys
+
+
 class BoardGame:
-    time = 20
+    time = 3
     min_players = 2
     max_players = 5
 
@@ -12,27 +16,50 @@ class BoardGame:
     def __init__(self, name):
         self.name = name
 
-    def gettime(self):
+    def get_time(self):
         return self.time
 
     def get_min(self):
         return self.min_players
+
+    def get_max(self):
+        return self.max_players
 # BoardGame
 
-
-def file_len(fname):
-    with open(fname) as f:
-        for j, l in enumerate(f):
-            pass
-    return j + 1
-
+# should be dynamic...
 num_lines = 158
+
 games_file = open("drawit_games.txt", 'r', encoding="utf-16")
 games_list = []
+prefered_games_list = []
+arg_numplayers = -1
+arg_time = -1
+arglist = sys.argv
+
+# get args
+if len(arglist) >= 2:
+    arg_numplayers = int(arglist[1])
+    arg_time = int(arglist[2])
+
+# add all games from file to games_list
 for i in range(0, num_lines):
     games_list.append(BoardGame(games_file.readline()))
 
-for i in range(0, num_lines):
-    print(games_list[i].get_min())
+# add games to prefered_games_list based on args
+for game in games_list:
+    if game.time <= arg_time\
+            and game.min_players <= arg_numplayers <= game.max_players:
+        prefered_games_list.append(game)
+    elif arg_numplayers == -1 and arg_time == -1:   # could be optimized better
+        prefered_games_list.append(game)
 
+# creates random int if needed
+rand_int = 0
+if len(prefered_games_list) > 1:
+    rand_int = random.randint(0, len(prefered_games_list)-1)
 
+# prints random game if found
+if len(prefered_games_list) > 0:
+    print(prefered_games_list[rand_int].name)
+else:
+    print("No such game")
