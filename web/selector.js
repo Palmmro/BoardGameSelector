1d3dc68e261a42b1150b1631c11a943b0f6d9149
Palmmro <palmro@student.chalmers.se>
2017-08-04 18:15:42 +0200

    display a random game when button is pressed

--- web/selector.js
+++ web/selector.js
@@ -6,7 +6,7 @@ var out = "";
 var dataTable;
 var rows = 1;
 var cols = 1;
-var loaded = false;
+var games = [rows-1];
 
 function loadData() {
     console.log("Click");
@@ -16,14 +16,13 @@ function loadData() {
     xmlhttp.onreadystatechange = function() {
         if(xmlhttp.readyState == 4 && xmlhttp.status==200){
             out = xmlhttp.responseText;
-            loaded = true;
-            // console.log("out1: " +out);
-            // document.getElementById("display").innerHTML = out;
+            updateTableSize();
+            dataTable = createArray(rows,cols);
+            loadTable();
         }
     };
-    xmlhttp.open("GET",url,true);
+    xmlhttp.open("GET",url,false);  //depricated need to set async to true
     xmlhttp.send(null);
-
 
 }
 
@@ -35,7 +34,6 @@ function createArray(length) {
         var args = Array.prototype.slice.call(arguments, 1);
         while(i--) arr[length-1 - i] = createArray.apply(this, args);
     }
-
     return arr;
 }
 
@@ -46,14 +44,10 @@ function loadTable(){
     var col = 0;
 
     for(var i = 0, len = out.length; i < len; i++){
-
-
         if(out[i] == "\t"){
-
             dataTable[row][col] = out.substring(currentStart,i);
             currentStart = i+1;
             col++;
-
         }
         if(out[i] == "\n"){
             dataTable[row][col] = out.substring(currentStart,i);
@@ -65,8 +59,6 @@ function loadTable(){
             dataTable[row][col] = out.substring(currentStart,i+1);
         }
     }
-
-    console.log(dataTable);
 }
 
 function updateTableSize(){
@@ -74,30 +66,42 @@ function updateTableSize(){
     var nrEnt = 0;
 
     for(var i = 0, len = out.length; i < len; i++){
-
         if(out[i] == "\t"){
             nrTab++;
-
         }
         if(out[i] == "\n"){
             nrEnt++;
         }
     }
-
     rows = nrEnt +1;
     cols = (nrTab+rows)/rows;
+}
+
+function fillGameArray() {
+    for(var i = 1; i < rows; i++){
+        var tempGame = {
+            name:dataTable[i][0],
+            length:dataTable[i][1],
+            minplayers:dataTable[i][2],
+            maxplayers:dataTable[i][3]
+        };
+        games[i-1] = tempGame;
+    }
 }
 
 
 function onClickButton() {
     loadData();
+    fillGameArray()
 
-    updateTableSize();
-    dataTable = createArray(rows,cols);
-    loadTable();
-
+    var random = Math.floor((Math.random() * (games.length-1) ));
+    var textField1 = document.getElementById("textField1");
+    textField1.innerHTML = "<p>"+games[random].name+"</p>";
 
 }
+
+
+
 
 
 
